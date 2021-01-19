9db3a43a3c1aac2e3ecc2f7461543d64f6ddf089
mboduroff <martinboduroff@gmail.com>
2021-01-19 11:43:47 +0200

    Using one more method for 'e1' object, formatting improvements.

--- January-21/4-Store/Products/Store.java
+++ January-21/4-Store/Products/Store.java
@@ -2,7 +2,8 @@ package Products;
 
 public class Store {
     public static void main(String[] args) {
-        System.out.println("Цената на електроенергията в лв е: " + ElectricalAppliance.getPriceOfElectricity());
+        System.out.println("Цената на електроенергията в лв е: " +
+                ElectricalAppliance.getPriceOfElectricity());
         Product p = new Product("Хладилник", 799.99, 60.2);
         ElectricalAppliance e = new ElectricalAppliance(p, 230, 150);
 
@@ -11,5 +12,7 @@ public class Store {
 
         ElectricalAppliance e1 = new ElectricalAppliance(p);
         e1.print();
+        e1.calculateCosts();
+
     }
 }
