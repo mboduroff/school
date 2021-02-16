3d8fd3900ea8cfcbf3e95e085405f41fa8ad2cc4
mboduroff <martinboduroff@gmail.com>
2021-02-16 12:29:01 +0200

    added comapring method

--- February-21/12-Fauna/fauna/TestFauna.java
+++ February-21/12-Fauna/fauna/TestFauna.java
@@ -2,7 +2,7 @@ package fauna;
 
 public class TestFauna {
     public static void main(String[] args) {
-        Creature cat1 = new Cat();
+        Cat cat1 = new Cat();
 
         System.out.println("Info about my first cat:");
         cat1.printInfo();
@@ -14,6 +14,18 @@ public class TestFauna {
         cat2.setAge(6);
         System.out.println("Info about my second cat: \n" + cat2.getInfo());
 
+        printBreedOfBiggerCat(cat1, cat2);
+    }
 
+    public static void printBreedOfBiggerCat(Cat c1, Cat c2) {
+        System.out.println("Comparing two cats:\n" +
+                "Cat 1:" + c1 + "\nCat 2: " + c2);
+        String temp;
+        if (c1.getSizeInCm() > c2.getSizeInCm())
+            temp = c1.getBreed();
+        else
+            temp = c2.getBreed();
+
+        System.out.println("The breed of the bigger cat is " + temp);
     }
 }
