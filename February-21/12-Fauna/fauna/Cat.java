cb772e5823184183bc3fcb3dbbb25bdfe24db7c2
mboduroff <martinboduroff@gmail.com>
2021-02-16 12:18:19 +0200

    New class: Cat

---
+++ February-21/12-Fauna/fauna/Cat.java
@@ -0,0 +1,48 @@
+package fauna;
+
+public class Cat extends Mammal {
+    private String name;
+    private String breed;
+
+    public Cat(boolean hasFur, String region, String name, String breed) {
+        super(hasFur, region);
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public Cat() {
+        System.out.println("===< New cat >===");
+        System.out.println("Name:");
+        this.name = sc.nextLine();
+        System.out.println("Breed: ");
+        this.breed = sc.nextLine();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    String getInfo() {
+        return super.toString() + String.format("%nName: %s | Breed: %s",
+                this.getName(),
+                this.getBreed());
+    }
+}
