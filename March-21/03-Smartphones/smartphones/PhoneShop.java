329c0436ea9cdb94399ce63de456a4a3792d76d5
mboduroff <martinboduroff@gmail.com>
2021-03-05 09:56:19 +0200

    Added console-reading functionality

--- March-21/03-Smartphones/smartphones/PhoneShop.java
+++ March-21/03-Smartphones/smartphones/PhoneShop.java
@@ -1,7 +1,11 @@
 package smartphones;
 
+import java.util.Scanner;
+
 public class PhoneShop {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Smartphone smartphone = new Smartphone();
 
         smartphone.browse("aol.com");
@@ -9,5 +13,12 @@ public class PhoneShop {
 
         smartphone.call("0888888888");
         smartphone.call("08888888B8");
+
+        System.out.println("\nEnter a website you'd like to visit:");
+        smartphone.browse(sc.nextLine().trim());
+
+        System.out.println("\nEnter a number you'd like to call:");
+        smartphone.call(sc.nextLine().trim());
     }
+
 }
