de41451d45b05039302737b52964329f132543d1
mboduroff <martinboduroff@gmail.com>
2021-03-12 11:10:27 +0200

    Task 3: Triangles, again.

---
+++ March-21/07-Triangles/triangles/TriangleAgain.java
@@ -0,0 +1,63 @@
+package triangles;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class TriangleAgain {
+    private double sideA;
+    private double sideB;
+    private int angleGamma;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public TriangleAgain(double sideA, double sideB, int angleGamma) {
+        if (sideA < 0 || sideB < 0 || angleGamma < 0) {
+            System.out.println("Грешка! Мерките не могат да бъдат отрицателни числа.");
+        } else {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.angleGamma = angleGamma;
+        }
+    }
+
+    public TriangleAgain() {
+        System.out.println("===< Нов триъгълник >===\n" +
+                "Моля, въведете следните мерки на триъгълника: ");
+        System.out.println("Страна а (cm): ");
+
+        double a = 0, b = 0;
+        int gamma = 0;
+
+        try {
+            a = sc.nextDouble();
+            System.out.println("Страна b (cm): ");
+            b = sc.nextDouble();
+            System.out.println("Ъгъл гама (градуса): ");
+            gamma = sc.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("Грешка! Ъгълът трябва да е цяло число.");
+        } catch (InputMismatchException e) {
+            System.out.println("Грешка! Моля, въвеждайте само числа.");
+        } finally {
+            if (sideA < 0 || sideB < 0 || angleGamma < 0) {
+                System.out.println("Грешка! Мерките не могат да бъдат отрицателни числа.");
+            } else {
+                this.sideA = a;
+                this.sideB = b;
+                this.angleGamma = gamma;
+            }
+        }
+    }
+
+    double findArea() {
+        double sideC = Math.sqrt(
+                Math.pow(this.sideA, 2) + Math.pow(this.sideB, 2)
+                - 2 * this.sideA * this.sideB *
+                Math.cos(Math.toRadians(this.angleGamma))
+        );
+
+        double p = (this.sideA + this.sideB + sideC) / 2;
+
+        return Math.sqrt(p * (p - this.sideB) * (p - this.sideB) * (p - sideC));
+    }
+}
---
+++ March-21/07-Triangles/triangles/TrianglesMainAgain.java
@@ -0,0 +1,8 @@
+package triangles;
+
+public class TrianglesMainAgain {
+    public static void main(String[] args) {
+        TriangleAgain t = new TriangleAgain(4.4, 5.2, 40);
+        System.out.println(t.findArea());
+    }
+}
