5aae2d9b2804bc696760608f4fe8c1510b3888f4
mboduroff <martinboduroff@gmail.com>
2021-03-22 10:33:02 +0200

    Task 1: write students to file

---
+++ March-21/09-Students/studentfiles/Student.java
@@ -0,0 +1,38 @@
+package studentfiles;
+
+public class Student {
+    private String name;
+    private int ageInYears;
+
+    public Student(String name, int ageInYears) {
+        this.name = name;
+        this.ageInYears = ageInYears;
+    }
+
+    void print() {
+        System.out.println(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAgeInYears() {
+        return ageInYears;
+    }
+
+    public void setAgeInYears(int ageInYears) {
+        this.ageInYears = ageInYears;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student:\t name: %s | age: %d y.",
+                this.getName(),
+                this.getAgeInYears());
+    }
+}
---
+++ March-21/09-Students/studentfiles/StudentsMain.java
@@ -0,0 +1,30 @@
+package studentfiles;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentsMain {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Ivan", 12));
+        studentList.add(new Student("Petur", 14));
+        studentList.add(new Student("Vanya", 16));
+        studentList.add(new Student("Petya", 18));
+        studentList.add(new Student("Georgi", 15));
+        studentList.add(new Student("Gergana", 13));
+        studentList.add(new Student("John", 9));
+        studentList.add(new Student("Jane", 8));
+        studentList.add(new Student("Jim", 8));
+        studentList.add(new Student("Steve", 8));
+
+        try (PrintWriter out = new PrintWriter("D:\\Git\\March-21\\09-Students\\studentfiles\\students.txt")) {
+            for (Student student : studentList) {
+                out.println(student);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файлът не е открит!");
+        }
+    }
+}
